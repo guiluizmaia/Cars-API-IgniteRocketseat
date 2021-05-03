9271c1d9668ed3ed06379d46c6a4d600e9a602e7
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-05-02 23:44:07 -0300

    token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,9 +1,20 @@
 import { IUsersRepository } from "modules/accounts/repositories/IUsersRepository";
 import { inject, injectable } from "tsyringe";
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken'
+
 
 interface IRequest {
     email: string;
     password: string;
+}
+
+interface IResponse{
+    user: {
+        name: string,
+        email: string,
+    };
+    token: string
 }
 
 @injectable()
@@ -13,12 +24,34 @@ class AuthenticateUserUseCase{
         private usersRepository: IUsersRepository
     ){}
 
-    async execute({ email, password }: IRequest){
+    async execute({ email, password }: IRequest): Promise<IResponse>{
         const user = await this.usersRepository.findByEmail(email);
 
         if(!user){
-            throw new Error("User not found")
+            throw new Error("Email or password not incorrect!");
         }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch){
+            throw new Error("Email or password not incorrect!");
+        }
+
+        const token = sign({}, "2446e1d6710e403270bec14f4188fc7c", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        return tokenReturn
+
     }
 }
 

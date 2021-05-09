8fe8f657974139405ed31f8badc5c6b2dc8d7b0d
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-05-08 22:24:59 -0300

    teste

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,9 @@
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+
+describe("Create Category", () =>{
+    
+    it("should be able to create a new category", () =>{
+        const createCategory = new CreateCategoryUseCase();
+    })
+})

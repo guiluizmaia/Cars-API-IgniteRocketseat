3e42f30a1ad91ac7cfda0d666d99e97c4121fb5b
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-05-12 00:40:06 -0300

    test

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { ICreateUserDTO } from "modules/accounts/dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+
+describe("Authenticate User", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    })
+    it("should be able to authenticate an user", async () =>{
+        const user: ICreateUserDTO = {
+            driver_license: "000123",
+            email: "user@teste.com",
+            password: "1234",
+            name: "user teste"
+        }
+
+        await createUserUseCase.execute(user);
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password,
+        })
+
+        console.log(result)
+    });
+})

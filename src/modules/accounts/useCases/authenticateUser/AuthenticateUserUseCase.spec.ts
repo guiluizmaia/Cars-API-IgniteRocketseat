a8ab58ee19d5c985a20f5d131e7508188cbe9583
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-05-13 00:08:50 -0300

    errors

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,5 +1,6 @@
-import { ICreateUserDTO } from "modules/accounts/dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../../accounts/dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../../accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 
@@ -30,5 +31,33 @@ describe("Authenticate User", () => {
         })
 
         console.log(result)
+        expect(result).toHaveProperty("token");
+    });
+
+    it("should not be able to authenticate an nonexistent user", () =>{
+        expect(async () =>{
+            await authenticateUserUseCase.execute({
+                email: "false@email.co",
+                password: "teste",
+            })
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to authenticate with incorrect password", () =>{
+        expect(async () =>{
+            const user: ICreateUserDTO ={
+                driver_license:"9999",
+                email:"user@user.com",
+                password: "1234",
+                name: "user"
+            }
+
+            await createUserUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "teste",
+            })
+        }).rejects.toBeInstanceOf(AppError);
     });
 })

fddfe94b11feaf7e3e954d0ed44b0808d7b40f91
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-08-22 10:56:59 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import "dotenv/config";
 import "express-async-errors";
+import cors from "cors";
+
 import createConnection from "../typeorm";
 import "../../container/index";
 
@@ -24,6 +26,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) =>{
